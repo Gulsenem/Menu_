45cc165423cc21d81225b2a6d81595f5aae242eb
Gulsenem <og.durdyeva@gmail.com>
2021-06-15 14:43:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,9 @@ var is_btn_akt = false;
 
 function aktivieren(element)
 {
-    var aktiv = document.createElement("div");
-    aktiv.classList.add("aktiv");
 
-    if (!is_btn_akt)
-    {
-        is_btn_akt = true;
-    }
-    else
-    {}
+    var aktiv = document.createElement("div");
+    aktiv.classList.add("aktiv");  
+  
 
 }
